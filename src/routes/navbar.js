145f4031146ecca622f6a4824cfe9674ea90c3a1
mohamad-ridwan <mr643062@gmail.com>
2022-01-08 09:10:00 +0700

    ridwan tes push to git

---
+++ src/routes/navbar.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const router = express.Router()
+
+const useControllers = require('../controllers/navbar')
+
+router.post('/post/logo-web')
+
+module.exports = router
